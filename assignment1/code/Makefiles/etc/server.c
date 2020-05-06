566b6bf0150b5e74183056ca664e9029c02924a4
Jai <jai.chhatre@gmail.com>
2020-05-06 12:52:06 -0700

    added new screenshot, added comments and made few changes to server.c

--- assignment1/code/Makefiles/etc/server.c
+++ assignment1/code/Makefiles/etc/server.c
@@ -7,89 +7,81 @@
 #include <string.h> 
 #define PORT 80
 
+
+int main(int argc, char const *argv[]) 
+{ 
     int server_fd, new_socket, valread; 
     struct sockaddr_in address; 
     int opt = 1; 
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
     char *hello = "Hello from server"; 
-
-       
-    int createSocket(){
-        
-        // Creating socket file descriptor 
-        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-        { 
-            perror("socket failed"); 
-            exit(EXIT_FAILURE); 
-        } 
-           
-        // Forcefully attaching socket to the port 8080 
-        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt)))
-        { 
-            perror("setsockopt"); 
-            exit(EXIT_FAILURE); 
-        } 
-        address.sin_family = AF_INET; 
-        address.sin_addr.s_addr = INADDR_ANY; 
-        address.sin_port = htons( PORT ); 
-           
-        // Forcefully attaching socket to the port 8080 
-        if (bind(server_fd, (struct sockaddr *)&address,  
-                                     sizeof(address))<0) 
-        { 
-            perror("bind failed"); 
-            exit(EXIT_FAILURE); 
-        } 
-        if (listen(server_fd, 3) < 0) 
-        { 
-            perror("listen"); 
-            exit(EXIT_FAILURE); 
-        } 
-
-        return 0;
-    }
-
     pid_t pid; 
 
-    int processDataFromClient(){
-        //accept connection from client and process data from client
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                           (socklen_t*)&addrlen))<0) 
-        { 
-            perror("accept"); 
-            exit(EXIT_FAILURE); 
-        } 
-
-        pid = fork();
-        if(pid == 0) //if child process
-        {
-            close(server_fd); 
-
-            if(setuid(-1) != 0) //if drop privileges is not successful
-                exit(EXIT_FAILURE); 
-
-            valread = read( new_socket , buffer, 1024);
-            printf("%s\n",buffer );
-            send(new_socket , hello , strlen(hello) , 0 );
-            printf("Hello message sent\n");
-
-        }
-
-        close(new_socket); 
-
-        if(pid < 0){ // error in creating child process
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-
-        return 0;
-    }
-
-
-    int main(int argc, char const *argv[]) 
+    // --------------------------- Part 1: Set up socket ---------------------------
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
-        createSocket();
-        processDataFromClient();
-        return 0;
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 80
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt)))
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 80
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    //accept connection from client and process data from client
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
     }
+
+//--------------------------- Part 2: Process data from client ---------------------------
+
+    pid = fork();
+
+    if(pid == 0) //if child process
+    {
+        printf("In child process\n");
+        close(server_fd); 
+        int setuidVal = setuid(-1); //drop privileges by setuid to "nobody"
+
+        if(setuidVal != 0) //if drop privileges is not successful
+            exit(EXIT_FAILURE); 
+
+        valread = read( new_socket , buffer, 1024); //process data from client
+        printf("%s\n",buffer );
+        send(new_socket , hello , strlen(hello) , 0 );
+        printf("Hello message sent\n");
+
+    }
+
+    close(new_socket); 
+
+    if(pid < 0){ // error in creating child process
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
