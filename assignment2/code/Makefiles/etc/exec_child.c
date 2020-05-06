de2cd33f6a73fec474dda55c8235c0f5e3529f1f
Jai <jai.chhatre@gmail.com>
2020-05-06 14:28:38 -0700

    added assignment 2

---
+++ assignment2/code/Makefiles/etc/exec_child.c
@@ -0,0 +1,31 @@
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <sys/wait.h>
+#define PORT 80
+int main(int argc, char const *argv[]) 
+{
+	printf("In exec'd child process\n");
+
+	int passed_socket_descriptor;
+    const char* hello_msg;
+
+	if(argc > 1){
+        passed_socket_descriptor=*argv[1];
+		hello_msg=argv[2];
+	}
+	else
+	{
+		printf("Error...\n");
+		exit(0);
+	}
+	char buffer[1024] = {0};
+	int valread = read( passed_socket_descriptor ,buffer, 1024); 
+	printf("%s\n",buffer ); 
+	send(passed_socket_descriptor , hello_msg , strlen(hello_msg) , 0 ); 
+	printf("Hello message sent from exec'd child\n"); 
+	return 0;
+}
---
+++ assignment2/code/Makefiles/etc/server.c
@@ -0,0 +1,89 @@
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 80
+
+
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+    pid_t pid; 
+
+    // --------------------------- Part 1: Set up socket ---------------------------
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 80
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt)))
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 80
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    //accept connection from client and process data from client
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    }
+
+//--------------------------- Part 2: Re-exec the server’s child process after forking ---------------------------
+
+    pid = fork();
+
+    if(pid == 0) //if child process
+    {
+        printf("In child process\n");
+        close(server_fd); 
+        int setuidVal = setuid(-1); //drop privileges by setuid to "nobody"
+
+        if(setuidVal != 0) //if drop privileges is not successful
+            exit(EXIT_FAILURE); 
+
+        //pass the socket file descriptor between the forked child and the new exec’ed child as command line arguments
+        if(execl("exec_child","exec_child",&new_socket,hello,NULL) < 0)
+        {
+            printf("exec error\n");
+            exit(0);
+        }
+
+    }
+
+    close(new_socket); 
+
+    if(pid < 0){ // error in creating child process
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
